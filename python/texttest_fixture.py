19046e3fbf923f162a18ffc565cae904cc827cc0
Nic Desjardins <nic@nicdesjardins.com>
2021-09-29 15:29:23 -0400

    Switched texttest_fixture to display using tabulate.

--- python/texttest_fixture.py
+++ python/texttest_fixture.py
@@ -2,29 +2,30 @@
 from __future__ import print_function
 
 from gilded_rose import *
+from tabulate import tabulate
 
 if __name__ == "__main__":
-    print ("OMGHAI!")
     items = [
              Item(name="+5 Dexterity Vest", sell_in=10, quality=20),
-             Item(name="Aged Brie", sell_in=2, quality=0),
+             Item(name=GildedRoseConstants.AGED_BRIE, sell_in=2, quality=0),
              Item(name="Elixir of the Mongoose", sell_in=5, quality=7),
-             Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
-             Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
-             Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
-             Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
-             Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
+             Item(name=GildedRoseConstants.SULFURAS, sell_in=0, quality=80),
+             Item(name=GildedRoseConstants.SULFURAS, sell_in=-1, quality=80),
+             Item(name=GildedRoseConstants.BACKSTAGE_PASSES, sell_in=15, quality=20),
+             Item(name=GildedRoseConstants.BACKSTAGE_PASSES, sell_in=10, quality=49),
+             Item(name=GildedRoseConstants.BACKSTAGE_PASSES, sell_in=5, quality=49),
              Item(name="Conjured Mana Cake", sell_in=3, quality=6),  # <-- :O
             ]
 
-    days = 2
+    days = 3
     import sys
     if len(sys.argv) > 1:
         days = int(sys.argv[1]) + 1
     for day in range(days):
         print("-------- day %s --------" % day)
-        print("name, sellIn, quality")
+        table = []
         for item in items:
-            print(item)
+            table.append([item.name, item.sell_in, item.quality])
+        print(tabulate(table, headers=["Item","Sell In", "Quality"]))
         print("")
         GildedRose(items).update_quality()
