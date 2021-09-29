36931ab1286f7dece1b0b47e226f36ebc1ad4d0e
Nic Desjardins <nic@nicdesjardins.com>
2021-09-29 16:20:06 -0400

    Updating fixtures to reflect moved constants directly into GildedRose class.

--- python/texttest_fixture.py
+++ python/texttest_fixture.py
@@ -7,13 +7,13 @@ from tabulate import tabulate
 if __name__ == "__main__":
     items = [
              Item(name="+5 Dexterity Vest", sell_in=10, quality=20),
-             Item(name=GildedRoseConstants.AGED_BRIE, sell_in=2, quality=0),
+             Item(name=GildedRose.AGED_BRIE, sell_in=2, quality=0),
              Item(name="Elixir of the Mongoose", sell_in=5, quality=7),
-             Item(name=GildedRoseConstants.SULFURAS, sell_in=0, quality=80),
-             Item(name=GildedRoseConstants.SULFURAS, sell_in=-1, quality=80),
-             Item(name=GildedRoseConstants.BACKSTAGE_PASSES, sell_in=15, quality=20),
-             Item(name=GildedRoseConstants.BACKSTAGE_PASSES, sell_in=10, quality=49),
-             Item(name=GildedRoseConstants.BACKSTAGE_PASSES, sell_in=5, quality=49),
+             Item(name=GildedRose.SULFURAS, sell_in=0, quality=80),
+             Item(name=GildedRose.SULFURAS, sell_in=-1, quality=80),
+             Item(name=GildedRose.BACKSTAGE_PASSES, sell_in=15, quality=20),
+             Item(name=GildedRose.BACKSTAGE_PASSES, sell_in=10, quality=49),
+             Item(name=GildedRose.BACKSTAGE_PASSES, sell_in=5, quality=49),
              Item(name="Conjured Mana Cake", sell_in=3, quality=6),  # <-- :O
             ]
 
