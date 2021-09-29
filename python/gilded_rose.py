b69b9ffaf6f0f34c37084ab2151972cc65d13879
Nic Desjardins <nic@nicdesjardins.com>
2021-09-29 15:28:42 -0400

    Constants: - Added "constants" to cover off special items. - Added a "constant" to cover off maximum quality.
    Added nextDay() method as that makes more sense than update_quality()
    
    Created tests to cover off initial Gilded Rose Requirements.

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,38 +1,56 @@
 # -*- coding: utf-8 -*-
 
-class GildedRose(object):
+class GildedRose(object):    
 
+    SULFURAS = "Sulfuras, Hand of Ragnaros"
+    AGED_BRIE = "Aged Brie"
+    BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert"
+
+    MAXIMUM_QUALITY = 50
+    
     def __init__(self, items):
         self.items = items
+        self.correctCapInitialQualities()
 
+    def correctCapInitialQualities(self):
+        for item in self.items:
+            if item.quality > self.MAXIMUM_QUALITY:
+                item.quality = self.MAXIMUM_QUALITY
+
+    def nextDay(self):
+        self.update_quality()
+    
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
+            
+            if item.name != self.SULFURAS:
+                item.sell_in = item.sell_in - 1
+            
+            if item.name != self.AGED_BRIE and item.name != self.BACKSTAGE_PASSES:
                 if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
+                    if item.name != self.SULFURAS:
                         item.quality = item.quality - 1
             else:
-                if item.quality < 50:
+                if item.quality < self.MAXIMUM_QUALITY:
                     item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                    if item.name == self.BACKSTAGE_PASSES:
                         if item.sell_in < 11:
-                            if item.quality < 50:
+                            if item.quality < self.MAXIMUM_QUALITY:
                                 item.quality = item.quality + 1
                         if item.sell_in < 6:
-                            if item.quality < 50:
+                            if item.quality < self.MAXIMUM_QUALITY:
                                 item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
+            
             if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
+                if item.name != self.AGED_BRIE:
+                    if item.name != self.BACKSTAGE_PASSES:
                         if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
+                            if item.name != self.SULFURAS:
                                 item.quality = item.quality - 1
                     else:
                         item.quality = item.quality - item.quality
                 else:
-                    if item.quality < 50:
+                    if item.quality < self.MAXIMUM_QUALITY:
                         item.quality = item.quality + 1
 
 
@@ -43,4 +61,7 @@ class Item:
         self.quality = quality
 
     def __repr__(self):
+        return self.___str___
+
+    def __str__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -3,14 +3,95 @@ import unittest
 
 from gilded_rose import Item, GildedRose
 
-
-class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
+class GildedRoseNormalItemTest(unittest.TestCase):
+    
+    def testThatNameIsCorrectlySetAfterUpdateQuality(self):
+        items = [Item("foo", 10, 10)]
         gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        gilded_rose.nextDay() # update_quality()
+        self.assertEqual("foo", items[0].name)
 
-        
+    def testThatSellInDateReducesNextDay(self):
+        items = [Item("an item", 10, 30)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.nextDay()
+        self.assertEqual(9, items[0].sell_in)
+    
+    def testThatQualityReducesNextDay(self):
+        items = [Item("an item", 10, 30)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.nextDay()
+        self.assertEqual(29, items[0].quality)
+
+    def testThatQualityOfAnItemIsNeverMoreThanMaximumDefinedQuality(self):
+        items = [Item("an item", 10, 999)]
+        GildedRose(items)
+        self.assertLessEqual(items[0].quality, GildedRose.MAXIMUM_QUALITY)
+    
+    def testThatQualityNeverGoesBelowZero(self):
+        items = [Item("an item", 0, 0)]
+        gilden_rose = GildedRose(items)
+        gilden_rose.nextDay()
+        self.assertEqual(0, items[0].quality)
+
+class GildedRoseBackStagePassTest(unittest.TestCase):
+
+    def testThatBackStagePassesQualityIncreasesBy1AtMoreThan10DaysLeft(self):
+        items = [Item(GildedRose.BACKSTAGE_PASSES, 12, 25)]
+        gilden_rose = GildedRose(items)
+        gilden_rose.nextDay()
+        self.assertEqual(26, items[0].quality)
+    
+    def testThatBackStagePassesQualityIncreasesBy2AtLessThanOrEq10DaysLeft(self):
+        items = [Item(GildedRose.BACKSTAGE_PASSES, 11, 25)]
+        gilden_rose = GildedRose(items)
+        gilden_rose.nextDay()
+        self.assertEqual(27, items[0].quality)
+    
+    def testThatBackStagePassesQualityIncreasesBy3AtLessThanOrEq5DaysLeft(self):
+        items = [Item(GildedRose.BACKSTAGE_PASSES, 6, 25)]
+        gilden_rose = GildedRose(items)
+        gilden_rose.nextDay()
+        self.assertEqual(28, items[0].quality)
+    
+    def testThatBackStagePassesQualityDropsToZeroAfterConcert(self):
+        items = [Item(GildedRose.BACKSTAGE_PASSES, 1, 10)]
+        gilden_rose = GildedRose(items)
+        gilden_rose.nextDay()
+        self.assertEqual(13, items[0].quality)
+        gilden_rose.nextDay()
+        self.assertEqual(0, items[0].quality)
+        gilden_rose.nextDay()
+        self.assertEqual(0, items[0].quality)
+
+class GildedRoseSulfurasTest(unittest.TestCase):
+    
+    def testThatSulfurasQualityDoesntDecrease(self):
+        items = [Item(GildedRose.SULFURAS, 10, 50)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.nextDay()
+        self.assertEqual(50, items[0].quality)
+
+    def testThatSulfurasSellInDateDoesntDecrease(self):
+        items = [Item(GildedRose.SULFURAS, 10, 50)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.nextDay()
+        self.assertEqual(10, items[0].sell_in)
+
+class GildedRoseAgedBrieTest(unittest.TestCase):
+
+    def testThatAgedBrieSellInDateDecreasesNextDay(self):
+        items = [Item(GildedRose.AGED_BRIE, 10, 30)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.nextDay()
+        self.assertEqual(9, items[0].sell_in)
+    
+    def testThatAgedBrieQualityIncreasesNextDay(self):
+        items = [Item(GildedRose.AGED_BRIE, 10, 30)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.nextDay()
+        self.assertEqual(31, items[0].quality)
+
+
 if __name__ == '__main__':
     unittest.main()
